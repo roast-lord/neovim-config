5f87317f05fa6162c67af20f541967f030f005a8
roast-lord <roastlord.dev@gmail.com>
2024-06-24 06:25:50 -0300

    Change neotree toggle button.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -11,13 +11,13 @@ return {
     },
     cmd = "Neotree",
     keys = {
-        { "\\", ":Neotree reveal<CR>", { desc = "NeoTree reveal" } },
+        { "z", ":Neotree reveal<CR>", { desc = "NeoTree reveal" } },
     },
     opts = {
         filesystem = {
             window = {
                 mappings = {
-                    ["\\"] = "close_window",
+                    ["z"] = "close_window",
                 },
             },
         },
