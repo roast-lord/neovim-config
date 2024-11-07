dfda0fb90b8b870d0fe133948f0bebdc8dcc0199
roastlord <roastlord.dev@gmail.com>
2024-11-07 06:39:04 -0300

    Removed deprecated ruff_lsp.

--- init.lua
+++ init.lua
@@ -572,6 +572,7 @@ require("lazy").setup({
                     settings = {
                         pyright = {
                             disableOrganizeImports = true,
+                            disableTaggedHints = true,
                         },
                         python = {
                             analysis = {
@@ -583,13 +584,6 @@ require("lazy").setup({
                     },
                 },
                 taplo = {},
-                ruff_lsp = {
-                    single_file_support = false,
-                    settings = {
-                        configurationPreference = "filesystemFirst",
-                        organize_imports = true,
-                    },
-                },
                 ruff = {
                     init_options = {
                         settings = {
@@ -978,3 +972,31 @@ require("lazy").setup({
 -- vim: ts=2 sts=2 sw=2 et
 --
 require("colorizer").setup {}
+
+require("conform").setup {
+    format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+            return
+        end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+    end,
+}
+
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+    if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+    else
+        vim.g.disable_autoformat = true
+    end
+end, {
+    desc = "Disable autoformat-on-save",
+    bang = true,
+})
+vim.api.nvim_create_user_command("FormatEnable", function()
+    vim.b.disable_autoformat = false
+    vim.g.disable_autoformat = false
+end, {
+    desc = "Re-enable autoformat-on-save",
+})
