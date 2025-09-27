43155fab3442c7081cb0fcf87c8b574da64dbfc9
roastlord <roastlord.dev@gmail.com>
2025-09-27 20:39:46 -0300

    Added new config

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -13,6 +13,8 @@ return {
         go = { 'golangcilint' },
       }
 
+      lint.linters.sqlfluff.args = { 'lint', '--format=json' }
+
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
       -- instead set linters_by_ft like this:
       -- lint.linters_by_ft = lint.linters_by_ft or {}
